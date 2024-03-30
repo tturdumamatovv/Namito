25cc1b73a6a15319579fcb3b1759fbb5b8900bec
tturdumamatovv <muki2022@mail.ru>
2024-03-30 13:56:32 +0600

    try to fix statics

--- config/settings/base.py
+++ config/settings/base.py
@@ -179,7 +179,9 @@ STATIC_ROOT = str(BASE_DIR / "static")
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
-STATICFILES_DIRS = [str(BASE_DIR / "static")]
+STATICFILES_DIRS = [
+    str(APPS_DIR / "static")
+]
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
