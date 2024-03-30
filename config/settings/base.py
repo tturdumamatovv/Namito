f7fb9cbec6be69f669f4db2d1374427748ba07ea
tturdumamatovv <muki2022@mail.ru>
2024-03-30 15:41:09 +0600

    fixing

--- config/settings/base.py
+++ config/settings/base.py
@@ -175,7 +175,7 @@ MIDDLEWARE = [
 # STATIC
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-root
-STATIC_ROOT = str(BASE_DIR / "static")
+STATIC_ROOT = str(BASE_DIR / "namito" / "static")
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
