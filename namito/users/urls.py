c6fcb6560e7840aada6fd22bea7bb2dabdbf6fdd
tturdumamatovv <muki2022@mail.ru>
2024-04-30 09:41:02 +0600

    rebuild

--- namito/users/urls.py
+++ namito/users/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('login/', UserLoginView.as_view(), name='user_registration'),
     path('verify-code/', VerifyCodeView.as_view(), name='verify_code'),
     path('profile/', UserProfileUpdateView.as_view(), name='user-profile'),
-    path('addresses/create/', UserAddressCreateAPIView.as_view(), name='create_address'),
+    path('addresses/', UserAddressCreateAPIView.as_view(), name='create_address'),
     path('addresses/<int:pk>/update/', UserAddressUpdateAPIView.as_view(), name='update_address'),
     path('addresses/<int:pk>/delete/', UserAddressDeleteAPIView.as_view(), name='delete_address'),
 ]
