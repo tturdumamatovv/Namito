391847a5f83422c004679e72d24ed91cef4e3b1b
tturdumamatovv <muki2022@mail.ru>
2024-05-06 16:13:38 +0600

    a

--- namito/catalog/api/filters.py
+++ namito/catalog/api/filters.py
@@ -1,16 +1,33 @@
 import django_filters
 from django.db.models import Avg, Q
-
-from namito.catalog.models import Product
+from namito.catalog.models import Product, Size, Color
 
 
 class ProductFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(field_name="name", lookup_expr='icontains')
     min_price = django_filters.NumberFilter(field_name="variants__price", lookup_expr='gte')
     max_price = django_filters.NumberFilter(field_name="variants__price", lookup_expr='lte')
-    color = django_filters.CharFilter(field_name="variants__color__name", lookup_expr='iexact')
-    size = django_filters.CharFilter(field_name="variants__size__name", lookup_expr='iexact')
-    brand = django_filters.CharFilter(field_name="brand__name", lookup_expr='iexact')
+
+    # Используем MultipleChoiceFilter для фильтрации по цвету
+    color = django_filters.MultipleChoiceFilter(
+        field_name='variants__color__name',
+        choices=lambda: [(color.name, color.name) for color in Color.objects.all()],
+        lookup_expr='in'
+    )
+
+    # Используем MultipleChoiceFilter для фильтрации по размеру
+    size = django_filters.MultipleChoiceFilter(
+        field_name='variants__size__name',
+        choices=lambda: [(size.name, size.name) for size in Size.objects.all()],
+        lookup_expr='in'
+    )
+
+    brand = django_filters.MultipleChoiceFilter(
+        field_name="brand__name",
+        choices=lambda: [(brand, brand) for brand in Product.objects.values_list('brand__name', flat=True).distinct()],
+        lookup_expr='in'
+    )
+
     category = django_filters.CharFilter(field_name="category__name", lookup_expr='iexact')
     min_rating = django_filters.NumberFilter(method='filter_by_min_rating')
     has_discount = django_filters.BooleanFilter(method='filter_by_discount_presence')
@@ -47,13 +64,12 @@ class ProductFilter(django_filters.FilterSet):
     def qs(self):
         queryset = super().qs
 
-        # Apply filters from request
-        queryset = self.apply_filters(queryset)
-
         sort_by_discount = self.request.GET.get('sort_by_discount')
         if sort_by_discount:
             queryset = self.sort_by_discount(queryset, sort_by_discount)
 
+        # Применение остальных фильтров
+        queryset = self.apply_filters(queryset)
         return queryset.distinct()
 
     def apply_filters(self, queryset):
@@ -63,11 +79,14 @@ class ProductFilter(django_filters.FilterSet):
             if 'max_price' in self.request.GET:
                 queryset = queryset.filter(variants__price__lte=self.request.GET['max_price'])
             if 'color' in self.request.GET:
-                queryset = queryset.filter(variants__color__name__iexact=self.request.GET['color'])
+                colors = self.request.GET.getlist('color')
+                queryset = queryset.filter(variants__color__name__in=colors)
             if 'size' in self.request.GET:
-                queryset = queryset.filter(variants__size__name__iexact=self.request.GET['size'])
+                sizes = self.request.GET.getlist('size')
+                queryset = queryset.filter(variants__size__name__in=sizes)
             if 'brand' in self.request.GET:
-                queryset = queryset.filter(brand__name__iexact=self.request.GET['brand'])
+                brands = self.request.GET.getlist('brand')
+                queryset = queryset.filter(brand__name__in=brands)
             if 'category' in self.request.GET:
                 queryset = queryset.filter(category__name__iexact=self.request.GET['category'])
             if 'min_rating' in self.request.GET:
