21a39c7ed8ac5df9b0ec197bd3e6a1a03ff5b460
tturdumamatovv <muki2022@mail.ru>
2024-05-07 11:59:36 +0600

    total pages

--- namito/catalog/api/pagination.py
+++ namito/catalog/api/pagination.py
@@ -1,3 +1,5 @@
+import math
+
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
@@ -6,10 +8,12 @@ class CustomPageNumberPagination(PageNumberPagination):
     page_size = 20
 
     def get_paginated_response(self, data):
+        total_pages = math.ceil(self.page.paginator.count / self.page_size)
         return Response({
             'count': self.page.paginator.count,
             'next': self.get_next_link(),
             'previous': self.get_previous_link(),
             'products': data,
             'page_size': self.page_size,
+            'total_pages': total_pages
         })
