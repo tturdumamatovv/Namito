bc3f5ea34c707d9f077e5534ed5e8da574fefd70
maksat.niiazov <niiazov.maksat98@gmail.com>
2024-05-16 12:41:49 +0600

    translate admin

--- config/settings/base.py
+++ config/settings/base.py
@@ -27,7 +27,7 @@ PRODUCTION = True
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # though not all of them may be available with every OS.
 # In Windows, this must be set to your system time zone.
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Bishkek"
 # https://docs.djangoproject.com/en/dev/ref/settings/#language-code
 LANGUAGE_CODE = "ru"
 # https://docs.djangoproject.com/en/dev/ref/settings/#languages
