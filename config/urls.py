75fbeff043d939a30949f3d9075e76c92cc9b42c
maksat.niiazov <niiazov.maksat98@gmail.com>
2024-05-22 12:03:43 +0600

    categories for layout

--- config/urls.py
+++ config/urls.py
@@ -42,6 +42,7 @@ urlpatterns += [
     # DRF auth token
     # path("auth-token/", obtain_auth_token),
     path("", include("namito.openapi.urls")),
+    path('', TemplateView.as_view(template_name='catalog/index.html'))
 
     #                re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     # path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
