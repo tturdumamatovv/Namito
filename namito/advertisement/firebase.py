b2361172e1f603a04c7fc4b30977ca8dc25c1a6e
tturdumamatovv <muki2022@mail.ru>
2024-06-03 11:42:38 +0600

    new notification

--- namito/advertisement/firebase.py
+++ namito/advertisement/firebase.py
@@ -8,14 +8,22 @@ cred = credentials.Certificate('firebase_key.json')
 firebase_admin.initialize_app(cred)
 
 
-def send_firebase_notification(title, description, image_url=None):
+def send_firebase_notification(user, title, description, image_url=None):
+    if not user.receive_notifications:
+        logger.info(f"User {user} has disabled notifications.")
+        return
+
+    if not user.fcm_token:
+        logger.warning(f"User {user} does not have FCM token. Skipping notification.")
+        return
+
     message = messaging.Message(
         notification=messaging.Notification(
             title=title,
             body=description,
             image=image_url,
         ),
-        topic='all',
+        token=user.fcm_token,
     )
     try:
         response = messaging.send(message)
