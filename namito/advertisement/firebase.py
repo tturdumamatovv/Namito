dc5c4577cb3271f949b705a4e54bd41d79d1f8ae
tturdumamatovv <muki2022@mail.ru>
2024-06-03 12:02:27 +0600

    a

--- namito/advertisement/firebase.py
+++ namito/advertisement/firebase.py
@@ -2,14 +2,22 @@ import firebase_admin
 from firebase_admin import credentials, messaging
 import logging
 
+from namito.users.models import User
+
 logger = logging.getLogger(__name__)
 
 cred = credentials.Certificate('firebase_key.json')
 firebase_admin.initialize_app(cred)
 
 
-def send_firebase_notification(title, description, image_url=None, user=None):
-    if user and user.fcm_token and user.receive_notifications:
+def send_firebase_notification(title, description, image_url=None, user_id=None):
+    try:
+        user = User.objects.get(pk=user_id)
+    except User.DoesNotExist:
+        logger.error(f"User with id {user_id} does not exist")
+        return
+
+    if user.fcm_token and user.receive_notifications:
         message = messaging.Message(
             notification=messaging.Notification(
                 title=title,
