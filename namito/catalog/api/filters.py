2a7a490c176f7432ea9ec263b08ab18e9390ab9d
tturdumamatovv <muki2022@mail.ru>
2024-07-04 10:55:41 +0600

    optimizided

--- namito/catalog/api/filters.py
+++ namito/catalog/api/filters.py
@@ -22,8 +22,8 @@ class ProductFilter(django_filters.FilterSet):
     def filter_by_category_slug(self, queryset, name, value):
         category = Category.objects.filter(slug=value).first()
         if category:
-            descendant_categories = category.get_descendants(include_self=True)
-            return queryset.filter(category__in=descendant_categories)
+            descendant_categories = category.get_descendants(include_self=True).values_list('id', flat=True)
+            return queryset.filter(category_id__in=descendant_categories)
         return queryset.none()
 
     def filter_by_brands(self, queryset, name, value):
@@ -32,29 +32,27 @@ class ProductFilter(django_filters.FilterSet):
 
     def filter_by_colors(self, queryset, name, value):
         colors = value.split(',')
-        return queryset.filter(variants__color__id__in=colors)
+        return queryset.filter(variants__color__id__in=colors).distinct()
 
     def filter_by_sizes(self, queryset, name, value):
         sizes = value.split(',')
-        return queryset.filter(variants__size__name__in=sizes)
+        return queryset.filter(variants__size__name__in=sizes).distinct()
 
     def filter_by_min_rating(self, queryset, name, value):
         return queryset.annotate(
             avg_rating=Avg('reviews__rating')
-        ).filter(
-            Q(avg_rating__gte=value)
-        )
+        ).filter(avg_rating__gte=value)
 
     def filter_by_discount_presence(self, queryset, name, value):
         if value:
             return queryset.filter(
-                Q(variants__discount_value__gt=0) &
-                Q(variants__discount_type__isnull=False)
+                variants__discount_value__gt=0,
+                variants__discount_type__isnull=False
             ).distinct()
         else:
             return queryset.exclude(
-                Q(variants__discount_value__gt=0) &
-                Q(variants__discount_type__isnull=False)
+                variants__discount_value__gt=0,
+                variants__discount_type__isnull=False
             ).distinct()
 
     def sort_by_discount(self, queryset, order):
@@ -62,17 +60,13 @@ class ProductFilter(django_filters.FilterSet):
             return queryset.order_by('variants__discount_value').distinct()
         elif order == 'desc':
             return queryset.order_by('-variants__discount_value').distinct()
-        else:
-            return queryset
+        return queryset
 
     @property
     def qs(self):
         queryset = super().qs
-
-        # Применяем фильтры из запроса
         queryset = self.apply_filters(queryset)
 
-        # Обрабатываем сортировку по скидке, если требуется
         sort_by_discount = self.request.GET.get('sort_by_discount')
         if sort_by_discount:
             queryset = self.sort_by_discount(queryset, sort_by_discount)
@@ -80,24 +74,24 @@ class ProductFilter(django_filters.FilterSet):
         return queryset.distinct()
 
     def apply_filters(self, queryset):
-        if self.request:
-            if 'min_price' in self.request.GET:
-                queryset = queryset.filter(variants__discounted_price__gte=self.request.GET['min_price'])
-            if 'max_price' in self.request.GET:
-                queryset = queryset.filter(variants__discounted_price__lte=self.request.GET['max_price'])
-            if 'color_id' in self.request.GET:
-                queryset = self.filter_by_colors(queryset, 'color_id', self.request.GET.getlist('color_id'))
-            if 'size' in self.request.GET:
-                queryset = self.filter_by_sizes(queryset, 'size', self.request.GET.get('size'))
-            if 'brand' in self.request.GET:
-                queryset = self.filter_by_brands(queryset, 'brand', self.request.GET.get('brand'))
-            if 'category_slug' in self.request.GET:  # измененная часть
-                queryset = self.filter_by_category_slug(queryset, 'category_slug',
-                                                        self.request.GET.get('category_slug'))
-            if 'min_rating' in self.request.GET:
-                queryset = self.filter_by_min_rating(queryset, 'min_rating', float(self.request.GET['min_rating']))
-            if 'has_discount' in self.request.GET:
-                has_discount = self.request.GET['has_discount'].lower() == 'true'
-                queryset = self.filter_by_discount_presence(queryset, 'has_discount', has_discount)
+        params = self.request.GET
+
+        if 'min_price' in params:
+            queryset = queryset.filter(variants__discounted_price__gte=params['min_price'])
+        if 'max_price' in params:
+            queryset = queryset.filter(variants__discounted_price__lte=params['max_price'])
+        if 'color_id' in params:
+            queryset = self.filter_by_colors(queryset, 'color_id', params.getlist('color_id'))
+        if 'size' in params:
+            queryset = self.filter_by_sizes(queryset, 'size', params['size'])
+        if 'brand' in params:
+            queryset = self.filter_by_brands(queryset, 'brand', params['brand'])
+        if 'category_slug' in params:
+            queryset = self.filter_by_category_slug(queryset, 'category_slug', params['category_slug'])
+        if 'min_rating' in params:
+            queryset = self.filter_by_min_rating(queryset, 'min_rating', float(params['min_rating']))
+        if 'has_discount' in params:
+            has_discount = params['has_discount'].lower() == 'true'
+            queryset = self.filter_by_discount_presence(queryset, 'has_discount', has_discount)
 
         return queryset
