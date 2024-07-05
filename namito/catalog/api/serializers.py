8e94238bc2940001e9fc4b0b090914e55e998ace
tturdumamatovv <muki2022@mail.ru>
2024-07-05 11:20:56 +0600

    a

--- namito/catalog/api/serializers.py
+++ namito/catalog/api/serializers.py
@@ -91,20 +91,12 @@ class CategoryBySlugSerializer(CategorySerializer):
 
     def get_min_price(self, obj):
         products = self.get_products(obj)
-        prices = []
-        for product in products:
-            min_price = product.get('min_price')
-            if min_price is not None:
-                prices.append(min_price)
+        prices = [product.get('min_price') for product in products if product.get('min_price') is not None]
         return min(prices) if prices else None
 
     def get_max_price(self, obj):
         products = self.get_products(obj)
-        prices = []
-        for product in products:
-            max_price = product.get('max_price')
-            if max_price is not None:
-                prices.append(max_price)
+        prices = [product.get('max_price') for product in products if product.get('max_price') is not None]
         return max(prices) if prices else None
 
     def get_colors(self, obj):
